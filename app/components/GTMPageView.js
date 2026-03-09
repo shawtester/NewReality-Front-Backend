eb1ca8004591dc190127bb01efffac1285828c41
silver-crow07 <shubhamchaudharyexe@gami.com>
2026-03-09 20:14:35 +0530

    gtm tracking fixed

---
+++ app/components/GTMPageView.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function GTMPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "pageview",
+      page: url,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}

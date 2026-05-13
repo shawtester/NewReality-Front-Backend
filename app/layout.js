f325387ebaca104df1be759dc3227286828acb11
silver-crow07 <shubhamchaudharyexe@gmail.com>
2026-05-13 13:44:08 +0530

    GTM UPDATE

--- app/layout.js
+++ app/layout.js
@@ -45,13 +45,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* ✅ GLOBAL SCHEMA (CONTROLLED VIA CLIENT COMPONENT) */}
-        <SchemaScript />
-
-        {/* ✅ GTM */}
+      <head>
+        {/* ✅ GTM HEAD */}
         <Script
           id="gtm-head"
           strategy="afterInteractive"
@@ -60,13 +55,18 @@ export default function RootLayout({ children }) {
             w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
             var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
             f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-M25MQ9BL');`,
           }}
         />
+      </head>
 
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* ✅ GTM NOSCRIPT BODY */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-M25MQ9BL"
@@ -75,6 +75,9 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+
+        {/* ✅ GLOBAL SCHEMA (CONTROLLED VIA CLIENT COMPONENT) */}
+        <SchemaScript />
 
         {/* ✅ UI */}
         <Toaster />
